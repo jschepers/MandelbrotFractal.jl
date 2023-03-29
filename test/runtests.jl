66229d6f11d43617eefd08dd8b2785031535f10e
jschepers <judith.schepers@vis.uni-stuttgart.de>
2023-03-29 11:28:40 +0000

    deleted plotting test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,22 +4,3 @@ using Test
 @testset "MandelbrotFractal.jl" begin
     @test mandelbrot(0) == 0
 end
-
-@testset "Plotting" begin
-    using UnicodePlots
-
-    y = -1:0.01:1
-    x = -2:0.01:0.5
-
-
-    A = fill(0.0, length(x), length(y))
-
-    for ix = eachindex(x)
-        for iy = eachindex(y)
-            c = complex(x[ix], y[iy])
-            A[ix, iy] = abs(mandelbrot(c))
-        end
-    end
-
-    heatmap(A)
-end
