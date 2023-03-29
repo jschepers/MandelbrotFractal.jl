a19c7dc6540c97bf8fc1e2f3f2dd754507601cde
jschepers <judith.schepers@vis.uni-stuttgart.de>
2023-03-29 11:09:55 +0000

    copied content from manual MandebrotFractal

--- src/MandelbrotFractal.jl
+++ src/MandelbrotFractal.jl
@@ -1,5 +1,17 @@
 module MandelbrotFractal
 
-# Write your package code here.
-
+function mandelbrot(a)
+    z = 0
+    for i = 1:50
+        z = z^2 + a
+        if abs.(z) > 2
+            break
+        end
+    end
+    return z
 end
+
+
+export mandelbrot
+end
+
